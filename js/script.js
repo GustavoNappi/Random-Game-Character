87507327e84c09dc0d34714472fb99ac172c155a
GustavoNappi <gustavosouzanappi03@gmail.com>
2025-09-18 14:50:16 -0300

    Primeiro commit do projeto

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const idadeH = document.createElementById('idade')
+const nomeH = document.createElementById('nome')
+const sobrenomeH = document.createElementById('sobrenome')
+const alturaH = document.createElementById('altura')
+
+
+function makingP(nome, sobrenome,altura,idade){
+    return 
+    nome: nomeH
+    
+
+}
